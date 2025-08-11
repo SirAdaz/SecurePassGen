b1f3865492b6c307fbf6a47820ee9ff2ec28d2b9
Adaz SirAdaz <mrxptdrgame@gmail.com>
2025-08-11 10:55:17 +0200

    Modification de la fonction de génération de mot de passe pour accepter un paramètre de longueur, permettant ainsi de spécifier la taille du mot de passe généré.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import secrets
 import string
 
-def generate_password():
+def generate_password(length):
     characters = string.ascii_lowercase
     password = ""
 
-    for i in range(20):
+    for i in range(length):
         password += secrets.choice(characters)
     
     return password
 
-print("voici votre mot de passe : ", generate_password())
+print("voici votre mot de passe : ", generate_password(20))
 
 
 
