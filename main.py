743c95d7d5e73f6eb5f579d4452bb81dc142ed8b
Adaz SirAdaz <mrxptdrgame@gmail.com>
2025-08-11 11:16:17 +0200

    Modification de la fonction de génération de mot de passe pour accepter des paramètres supplémentaires permettant de spécifier l'inclusion de lettres majuscules, de chiffres et de caractères spéciaux.

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import secrets
 import string
 
-def generate_password(length):
-    safe_special_characters = "!@#$%^&*()-_=+"
+def generate_password(length, has_uppercase, has_digits, has_special_characters):
+    safe_special_characters = "!@#$%^&*-_=+"
     password = ""
-    characters = string.ascii_lowercase + string.ascii_uppercase + string.digits + safe_special_characters
+    characters = string.ascii_lowercase
+
+    if has_uppercase:
+        characters += string.ascii_uppercase
+    if has_digits:
+        characters += string.digits
+    if has_special_characters:
+        characters += safe_special_characters
 
     for i in range(length):
         password += secrets.choice(characters)
     
     return password
 
-print("voici votre mot de passe : ", generate_password(20))
+print("voici votre mot de passe : ", generate_password(20, True, True, True))
 
 
 
