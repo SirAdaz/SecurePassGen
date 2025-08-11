8a803cef2b2d6e76f93d3d8502da09098152cbdd
Adaz SirAdaz <mrxptdrgame@gmail.com>
2025-08-11 11:19:10 +0200

    Ajout de commentaires explicatifs dans la fonction de génération de mot de passe pour clarifier le code et améliorer sa lisibilité.

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import secrets
 import string
 
-def generate_password(length, has_uppercase, has_digits, has_special_characters):
-    safe_special_characters = "!@#$%^&*-_=+"
-    password = ""
-    characters = string.ascii_lowercase
+safe_special_characters = "!@#$%^&*-_=+" # caractères sécurisés
 
-    if has_uppercase:
+def generate_password(length, has_uppercase, has_digits, has_special_characters):
+    password = "" # mot de passe généré
+    characters = string.ascii_lowercase # caractères disponibles
+
+    if has_uppercase: # si l'utilisateur veut des majuscules
         characters += string.ascii_uppercase
-    if has_digits:
+    if has_digits: # si l'utilisateur veut des chiffres
         characters += string.digits
-    if has_special_characters:
+    if has_special_characters: # si l'utilisateur veut des caractères spéciaux
         characters += safe_special_characters
 
-    for i in range(length):
+    for i in range(length): # génération du mot de passe
         password += secrets.choice(characters)
     
     return password
 
-print("voici votre mot de passe : ", generate_password(20, True, True, True))
+print("voici votre mot de passe : ", generate_password(20, True, True, True)) # appel de la fonction
 
 
 
